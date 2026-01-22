fc8c31d0a85ff88085d6acc3bd24d534b3500994
Adriell8000B <adrielluiz73@gmail.com>
2026-01-22 12:29:15 -0300

    Adde AddExam() types

--- interfaces/controller-interface.ts
+++ interfaces/controller-interface.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
+import { DefaultSchemaOptions, Document, Types } from "mongoose";
+import { IExam } from "../models/ExamModel";
 
 export interface IExamController {
   GetRoot(req: Request, res: Response): Response
   GetExams(req: Request, res: Response): Promise<Response>
+  AddExam(req: Request, res: Response): Promise<(Document<unknown, {}, IExam, {}, DefaultSchemaOptions> & IExam & Required<{
+    _id: Types.ObjectId;
+  }> & {
+    __v: number;
+  } & {
+    id: string;
+  }) | null>
 }
