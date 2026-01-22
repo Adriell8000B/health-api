4513435b07f27ce21224a32cd30da589bd488d55
Adriell8000B <adrielluiz73@gmail.com>
2026-01-22 12:29:41 -0300

    Added CreateExam() method

--- repositories/ExamRepository.ts
+++ repositories/ExamRepository.ts
@@ -12,7 +12,23 @@ class ExamRepository implements IExamRepository {
 
   public async RetrieveExams() {
     const [response, error] = await HandlePromise(this._exam_model.find({}))
-    if (error) {}
+    if (error) {
+      console.log(error)
+    }
+    return response
+  }
+
+  public async CreateExam(exam_type: string, exam_location: string, exam_date: string) {
+    const [response, error] = await HandlePromise(this._exam_model.create({
+      exam_type: exam_type,
+      exam_location: exam_location,
+      exam_date: exam_date
+    }))
+
+    if(error) {
+      console.log(error)
+    }
+
     return response
   }
 }
