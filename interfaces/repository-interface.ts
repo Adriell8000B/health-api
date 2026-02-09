35f769a0e40905ef56e6ffe8edb371cc39be76e9
Adriell8000B <adrielluiz73@gmail.com>
2026-02-09 19:03:37 -0300

    Refactor

--- interfaces/repository-interface.ts
+++ interfaces/repository-interface.ts
@@ -2,19 +2,7 @@ import { DefaultSchemaOptions, Document, Types } from "mongoose";
 import { IExam } from "../models/ExamModel";
 
 export interface IExamRepository {
-  RetrieveExams(exam_email:string): Promise<(Document<unknown, {}, IExam, {}, DefaultSchemaOptions> & IExam & Required<{
-    _id: Types.ObjectId;
-  }> & {
-    __v: number;
-  } & {
-    id: string;
-  })[] | null>
+  RetrieveExams(exam_email:string): Promise<IExam[] | null>
 
-  CreateExam(exam_email:string, exam_type: string, exam_location: string, exam_date: string): Promise<(Document<unknown, {}, IExam, {}, DefaultSchemaOptions> & IExam & Required<{
-    _id: Types.ObjectId;
-  }> & {
-    __v: number;
-  } & {
-    id: string;
-  }) | null>
+  CreateExam(exam_email:string, exam_type: string, exam_location: string, exam_date: string): Promise<IExam | null>
 }
